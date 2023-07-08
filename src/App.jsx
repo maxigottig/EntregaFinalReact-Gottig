9be902a6a259302ffbff8b07db04fb93876f569c
Maxi Gottig <maxigottig1985@gmail.com>
2023-07-07 23:26:09 -0300

    EntregaFinal

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Navbar from "./components/NavBar/NavBar"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ItemListContainerPage from './components/pages/ItemListContainerPage'
+import AboutPage from './components/pages/AboutPage'
+import ItemDetailContainerPage from './components/pages/ItemDetailContainerPage'
+import Header from "./components/Header/Header"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Router>
+    <Header />
+    <Navbar></Navbar>
+    
+    <Routes>
+    <Route path = "/" element={<ItemListContainerPage/>} />
+    <Route path = "/category/:category" element={<ItemListContainerPage />} />
+    <Route path = "/about" element={<AboutPage />} />
+    <Route path = "/item/:id" element={<ItemDetailContainerPage/>} />
+    </Routes>
+
+    </Router>    
+    </>
+  );
+};
+
+export default App
